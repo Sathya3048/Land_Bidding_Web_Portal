3a1bec107dd6901f026b590d574cb415dbb309a6
RavaliD17 <129702415+RavaliD17@users.noreply.github.com>
2023-04-16 22:25:50 -0500

    Add files via upload

---
+++ Land-Bidding (1)/Land-Bidding/src/test/java/com/bidding/land/LandBiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidding.land;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandBiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
