55190296ef9acb431e775de94716c6d3b8e1cfba
sudanigithub <129473391+sudanigithub@users.noreply.github.com>
2023-04-16 23:41:20 -0500

    Add files via upload

---
+++ LandBiddingApplication.java
@@ -0,0 +1,13 @@
+package com.bidding.land;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandBiddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandBiddingApplication.class, args);
+	}
+
+}
